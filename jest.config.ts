c1f099cfd5fbac95f9ca383fc75b2e7bfd5ea109
Benjamin Legrand <b.legrand@groupeonepoint.com>
2022-11-21 12:00:15 +0100

    test: fix jest config following upgrade,

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "jest";
+
+// eslint-disable-next-line no-undef
+globalThis.ngJest = {
+  skipNgcc: false,
+  tsconfig: "tsconfig.spec.json",
+};
+
+const esModules = [
+  "@angular",
+  "angular-split",
+  "typescript-logger",
+  "ng-bootstrap",
+  "@ngneat/falso",
+].join("|");
+
+const jestConfig: Config = {
+  preset: "jest-preset-angular",
+  globalSetup: "jest-preset-angular/global-setup",
+  setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
+  transformIgnorePatterns: [`<rootDir>/node_modules/(?!${esModules})`],
+};
+
+export default jestConfig;
