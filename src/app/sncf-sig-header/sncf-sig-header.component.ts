0220daaa10eee0cfb3575c3b2a0c36b9d00a012e
Benjamin Legrand <b.legrand@groupeonepoint.com>
2022-11-21 13:06:33 +0100

    chore(angular): upgrade to angular 15

--- src/app/sncf-sig-header/sncf-sig-header.component.ts
+++ src/app/sncf-sig-header/sncf-sig-header.component.ts
@@ -1,14 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
 import { Input } from "@angular/core";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: "sncf-sig-header",
   styleUrls: ["./sncf-sig-header.component.css"],
   templateUrl: "./sncf-sig-header.component.html",
   standalone: true,
-  imports: [CommonModule, NgbModule]
+  imports: [CommonModule]
 })
 export class SncfSigHeaderComponent implements OnInit {
 
